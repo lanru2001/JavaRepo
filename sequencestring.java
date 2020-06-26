e0b6cffdaaa1b02d2fe7e54f67e9b4df15559d3f
lanru2001 <lanru2001@yahoo.com>
2020-06-26 13:39:22 -0400

    First added commit

---
+++ sequencestring.java
@@ -0,0 +1,12 @@
+package com.temitopeolanrewaju81;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message="Hello World" + "!!!";
+        System.out.println(message.startsWith("Hello World"));
+    }
+}
+
